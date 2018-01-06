ca7559003a4b47966fdcb67f9c64951b9c6f8494
Techno-coder <zhiyuanqi@hotmail.co.nz>
2018-01-07 08:26:40 +1300

    Add CustomDesktopStorage class

---
+++ osu.Framework/Platform/CustomDesktopStorage.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+namespace osu.Framework.Platform {
+    public class CustomDesktopStorage : DesktopStorage {
+        internal static string basePath;
+
+        public CustomDesktopStorage(string baseName)
+            : base(baseName) { }
+
+        protected override string LocateBasePath() {
+            return basePath;
+        }
+    }
+
+    public class CustomDesktopStorageBuilder {
+        public string BasePath { get; set; }
+        public string BaseName { get; set; }
+
+        public CustomDesktopStorage Build() {
+            CustomDesktopStorage.basePath = BasePath;
+            return new CustomDesktopStorage(BaseName);
+        }
+    }
+}
